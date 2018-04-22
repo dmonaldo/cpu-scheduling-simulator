438d24c2d5c796da639d23f213d782b91888d2b6
agrsu <38569648+agrsu@users.noreply.github.com>
2018-04-21 18:52:00 -0700

    Update FCFS.cpp

--- FCFS.cpp
+++ FCFS.cpp
@@ -2,4 +2,11 @@
 
 virtual void FSFC::runScheduler()
 {
+  waitTime[0] = 0;
+  for(int i = 1; i < pidCount; i ++){
+    waitTime[i] = 0;
+    for(int j = 0 ; j < i; j++){
+      waitTime[i] += process[i].burst;
+    }
+  }
 }
