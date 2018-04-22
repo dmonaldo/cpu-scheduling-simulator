38e07f1b20ce704f92ab5773cebdee317a23a36f
agrsu <38569648+agrsu@users.noreply.github.com>
2018-04-21 18:14:37 -0700

    Update and rename scheduler.cpp to scheduler.h

--- scheduler.h
+++ scheduler.h
@@ -1,7 +1,13 @@
 // Abstract class for scheduler algorithms
+#ifndef SCHEDULER_H
+#define SCHEDULER_H
+
+#include <iostream>
 #include <vector>
+
 using namespace std
-struct{
+
+  struct{
   int pid;
   bool state;
   int arrival;
@@ -14,6 +20,11 @@ class Scheduler {
     ~Scheduler(void);
     virtual void parseInputFile() = 0;
     virtual void runScheduler() = 0;
+    const double avgTurnaroundQuery();
+    const double avgWaitQuery();
+    const double avgRespQuery();
+    const double cpuUseQuery();
+  
   protected:
     vector<int> waitTime;
     double avgWait;
@@ -21,7 +32,8 @@ class Scheduler {
     double avgResp;
     vector<int> turnTime;
     double avgTurn;
-    
+    double cupUsage;
+  
     // struct {
     //   int pid;
     //   bool state;
