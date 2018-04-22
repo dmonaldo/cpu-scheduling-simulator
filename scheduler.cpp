72e2551c5d73a9fe835e3860d472be968a5bcd97
agrsu <38569648+agrsu@users.noreply.github.com>
2018-04-21 18:06:42 -0700

    Update scheduler.cpp

--- scheduler.cpp
+++ scheduler.cpp
@@ -1,4 +1,12 @@
 // Abstract class for scheduler algorithms
+#include <vector>
+using namespace std
+struct{
+  int pid;
+  bool state;
+  int arrival;
+  int burst;
+}
 
 class Scheduler {
   public:
@@ -6,7 +14,14 @@ class Scheduler {
     ~Scheduler(void);
     virtual void parseInputFile() = 0;
     virtual void runScheduler() = 0;
-
+  protected:
+    vector<int> waitTime;
+    double avgWait;
+    vector<int> respTime;
+    double avgResp;
+    vector<int> turnTime;
+    double avgTurn;
+    
     // struct {
     //   int pid;
     //   bool state;
