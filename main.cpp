56709659f44e757bd60ee98f86ee26375b408bc9
agrsu <38569648+agrsu@users.noreply.github.com>
2018-04-22 18:19:38 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 
 using namespace std;
+enum scheAlgorithms {FCFS, RR, SRTF};
 
 int main(int argc, char *argv[]) {
   string inputFile;
   string scheduler;
   int timeQuantum;
 
+  /*
+  string commandLine;
+  stringstream iss(commandLine);
+  iss >> inputFile >> scheduler;
+  */
   // Check for minimum arguments to run
   if (argc < 3) {
     cout << "You are missing some arguments." << endl;
@@ -17,28 +23,35 @@ int main(int argc, char *argv[]) {
   scheduler = argv[2];
 
   // Determine scheduler algorithm to run
-  if (scheduler == "FCFS") {
-    cout << "FCFS" << endl;
-    // Initialize FCFS object here
-  }
-  else if (scheduler == "RR") {
-    cout << "RR" << endl;
-    if (argc < 4) {
-      cout << "You are missing the time quantum argument." << endl;
-      return 0;
-    } else {
-      timeQuantum = atoi(argv[3]);
-    }
-    // Initialize RR object here
-  }
-  else if (scheduler == "SRTF") {
-    cout << "SRTF" << endl;
-    // Initialize SRTF object here
-  }
-  else {
-    cout << "Invalid scheduler name." << endl;
+  switch(scheduler)
+  {
+    case scheAlgorithms.FCFS:
+      cout << "Scheduling algorithm: " << scheduler << endl;
+      // Initialize FCFS object here and then print out results
+      break;
+    case scheAlgorithms.RR:
+      cout << "Scheduling algorithm: " << scheduler << endl;
+      /*
+      iss >> inputFile >> scheduler >> timeQuantum;
+      
+      */
+      if (argc < 4) {
+        cout << "You are missing the time quantum argument." << endl;
+        return 0;
+      } else {
+        timeQuantum = atoi(argv[3]);
+      }
+      // Initialize RR object here and then print out results
+      break;
+    case scheAlgorithms.SRTF:
+      cout << "Scheduling algorithm: " << scheduler << endl;
+      // Initialize SRTF object here and then print out results 
+      break;
+    default:
+      cout << "Invalid scheduler name. Exiting program..." << endl;
     return 0;
   }
+
 
   return 0;
 }
