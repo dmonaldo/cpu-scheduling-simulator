427f5baaf6e981fb6434bb2138434b91148a3a6a
agrsu <38569648+agrsu@users.noreply.github.com>
2018-04-23 12:49:34 -0700

    Create SRTF.cpp

---
+++ SRTF.cpp
@@ -0,0 +1,14 @@
+#include "SRTF.h"
+
+virtual void SRTF::runScheduler()
+{
+}
+virtual double SRTF::avgRespQuery()
+{
+}
+virtual double SRTF::avgWaitQuery()
+{
+}
+virtual double avgTurnaroundQuery()
+{
+}
