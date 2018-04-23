9a28ea15e9725323be50a08de2d91bd9d900fc96
agrsu <38569648+agrsu@users.noreply.github.com>
2018-04-22 17:26:34 -0700

    Update scheduler.h

--- scheduler.h
+++ scheduler.h
@@ -19,18 +19,20 @@ class Scheduler {
     Scheduler(string input);
     ~Scheduler(void);
     virtual void parseInputFile();
-    virtual void runScheduler() = 0;
-    const double avgTurnaroundQuery();
-    const double avgWaitQuery();
-    const double avgRespQuery();
+    virtual void runScheduler() = 0; //bool?
+    void printResults();
+    
     const double cpuUseQuery();
   
   protected:
-    void printProcess();
+    virtual double avgTurnaroundQuery() = 0;
+    virtual double avgWaitQuery() = 0;
+    virtual double avgRespQuery() = 0;
+    void printCurrProcess();
     vector<processInfo> process;
     vector<int> waitTime;
     double avgWait;
-    vector<int> respTime;
+    //vector<int> respTime;
     double avgResp;
     vector<int> turnTime;
     double avgTurn;
