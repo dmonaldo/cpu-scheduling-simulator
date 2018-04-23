d60c5f60aa4c015b40353ac8570b6ebaa60aefcc
agrsu <38569648+agrsu@users.noreply.github.com>
2018-04-22 17:26:11 -0700

    Update FCFS.cpp

--- FCFS.cpp
+++ FCFS.cpp
@@ -2,11 +2,46 @@
 
 virtual void FSFC::runScheduler()
 {
-  waitTime[0] = 0;
-  for(int i = 1; i < pidCount; i ++){
-    waitTime[i] = 0;
-    for(int j = 0 ; j < i; j++){
-      waitTime[i] += process[i].burst;
+  parseInputFile();
+  
+  for(int i = 0 i < pidCount; i++){
+    for(int j = 0; j < process[i].burst; j++){
+      timeCounter++;
+      printRunProcess(i++);
     }
-  }
+    cout << "<system time " << timeCounter << "> process " 
+      << i++ << "is finished running... " << endl;
 }
+virtual double FCFS::avgRespQuery()
+{
+  avgResp = avgWaitQuery();
+  return avgResp;
+}
+virtual double FSFC::avgWaitQuery()
+{
+  vector<int> tempStart;
+  tempStart[0] = process[0].arrival;
+  for(int i = 1, j = 0; i < pidCount; i++, j++){
+      tempStart[i] = tempStart[j] + process[j].burst;
+  }
+  for(int i = 0; i < pidCount; i++){
+    wait[i] = tempStart[i] - process[i].arrival;
+    avgWait += wait[i];
+  }
+  avgWait \= pidCount++;
+  return avgWait;
+}
+virtual double FSFC::avgTurnaroundQuery()
+{
+  int tempBurst = 0;
+  int tempTurn = 0;
+  for(int i = 0; i < pidCount; i++){
+    tempBurst += process[i].burst;
+    tempTurn = tempBurst - process[i].arrival;
+    avgTurn += tempTurn;
+  }
+  avgTurn /= pidCount++;
+  return avgTurn;
+}
+  
+
